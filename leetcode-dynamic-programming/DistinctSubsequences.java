8c13a13a41a832260db2e856ff628aab72a6a3ae
ANWAR <122365112+anwaraltadfi@users.noreply.github.com>
2025-07-01 06:59:07 -0400

    Add Distinct Subsequences solution using DP

---
+++ leetcode-dynamic-programming/DistinctSubsequences.java
@@ -0,0 +1,37 @@
+// LeetCode Problem 115 - Distinct Subsequences
+// Solved using Dynamic Programming
+// Author: Anwar Altadfi
+
+public class DistinctSubsequences {
+
+    public static int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        if (n > m) return 0;
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s = "rabbbit";
+        String t = "rabbit";
+        System.out.println("Number of distinct subsequences: " + numDistinct(s, t));  // Output: 3
+    }
+}
