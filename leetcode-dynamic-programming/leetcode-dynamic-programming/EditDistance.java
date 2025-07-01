bad851f92dc4fbbd42c973f5e7c5547290a2e73c
ANWAR <122365112+anwaraltadfi@users.noreply.github.com>
2025-07-01 07:09:08 -0400

    Add Edit Distance solution using DP

---
+++ leetcode-dynamic-programming/leetcode-dynamic-programming/EditDistance.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // الحالة الابتدائية
+        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        for (int j = 0; j <= n; j++) dp[0][j] = j;
+
+        // بناء الجدول dp
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j],     // حذف  
+                                           Math.min(dp[i][j - 1], // إدراج  
+                                                    dp[i - 1][j - 1])); // استبدال  
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
